7e332309e2b59b8e2039329e7e7b29e279d52b3f
Yash <yashvardhandubey83@gmail.com>
2026-01-19 00:18:42 +0530

    updated NavBar

--- NoteSaver/src/App.jsx
+++ NoteSaver/src/App.jsx
@@ -3,14 +3,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import Paste from "./components/Paste";
 import ViewPaste from "./components/ViewPaste";
-import Navbar from "./components/Navbar";
 
+import NavBar from "./components/NavBar";
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <div className="w-full h-full flex flex-col">
-        <Navbar />
+        <NavBar />
         <Home />
       </div>
     ),
